71db931f580ede885c9cb3ca747ce1ad976a61c3
JoshuaViera <joshuaviera95@gmail.com>
2019-01-09 12:37:26 -0500

    Constraints set

---
+++ MockPeerProject/Views/MagicCell.swift
@@ -0,0 +1,13 @@
+//
+//  MagicCell.swift
+//  MockPeerProject
+//
+//  Created by Joshua Viera on 1/9/19.
+//  Copyright © 2019 Joshua Viera. All rights reserved.
+//
+
+import UIKit
+
+class MagicCell: UICollectionViewCell {
+    @IBOutlet weak var magicPhoto: UIImageView!
+}
---
+++ MockPeerProject/Views/PokemonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonCell.swift
+//  MockPeerProject
+//
+//  Created by Joshua Viera on 1/9/19.
+//  Copyright © 2019 Joshua Viera. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    @IBOutlet weak var pokemonImage: UIImageView!
+}
